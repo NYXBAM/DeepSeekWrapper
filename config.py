6aec22bfceac7268f6af16fba72ff41af53c4643
NYXBAM <delirium.dev@proton.me>
2026-01-25 10:59:16 -0800

    fixed: - login button, - path to storage

--- config.py
+++ config.py
@@ -3,30 +3,27 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-BROWSER_TYPE = "firefox"
+BROWSER_TYPE = "firefox" # or chromium 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 CONFIG = {
-        "storage_state_path": "storage/storage_state.json",
-        "protected_page_url": "https://chat.deepseek.com/",
-        "paragraph_selector": ".ds-markdown-paragraph, pre",
-        "last_paragraph_selector": ".ds-markdown-paragraph, pre",
-        "input_placeholder": "Message DeepSeek",
-        "button_combo_selector": "._7436101.ds-icon-button"
-    }
-    
-
-
-
-
+    "storage_state_path": os.path.join(BASE_DIR, "storage", "storage_state.json"),
+    "protected_page_url": "https://chat.deepseek.com/",
+    "paragraph_selector": ".ds-markdown-paragraph, pre",
+    "last_paragraph_selector": ".ds-markdown-paragraph, pre",
+    "input_placeholder": "Message DeepSeek",
+    "button_combo_selector": "._7436101.ds-icon-button"
+}
 
 auth_config = {
-    'storage_state_path': "storage/storage_state.json",
+    "storage_state_path": os.path.join(BASE_DIR, "storage", "storage_state.json"),
     'login_url': 'https://chat.deepseek.com/sign_in',
     'login': os.getenv("DEEPSEEK_LOGIN"),
     'password': os.getenv("DEEPSEEK_PASSWORD"),
     'user_input_placeholder': "Phone number / email address",
     'password_input_placeholder': "Password",
-    'unique_class': ".ds-sign-up-form__register-button"
+    'unique_class': 'button:has-text("Log in")'
 }
 
 
